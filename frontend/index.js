7181d2a26e27cd42b9b2e5f57a49637589716d7c
Meirzhan <carrieblackwood30@gmail.com>
2024-02-23 18:37:05 +0600

    good fixed

--- frontend/index.js
+++ frontend/index.js
@@ -10,19 +10,19 @@ let todoArray = []
 
 const URL = "http://localhost:3000/todos"
 
-async function getTodos(){
-    try{
+async function getTodos() {
+    try {
         const response = await fetch(URL)
         const data = response.json()
         return data
-    }catch(e){
+    } catch (e) {
         return e
     }
 }
 
-async function postTodos(){
-    try{
-        let options ={
+async function postTodos() {
+    try {
+        let options = {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
@@ -34,38 +34,29 @@ async function postTodos(){
         const response = await fetch(URL, options)
         const data = await response.json()
         return data
-    }catch(e){
+    } catch (e) {
         return e
     }
 }
 
-function displayTodos(json){
 
-    
 
-}
+createTodoBtn.addEventListener("click", async () => {
 
-getTodos()
-    .then(todoArr =>{
-        todoArray = todoArr
-        console.log(todoArray)
-        displayTodos(todoArray)
-    })
-    .catch(e => console.log(e))
+    await postTodos()
 
-createTodoBtn.addEventListener("click", () =>{
+    if (inputTodo.value != '') {
 
-    if(inputTodo.value != ''){
-        postTodos()
+        const activeDiv = document.querySelector('.active')
+        const finishedDiv = document.querySelector('.finished')
+        const onHoldDiv = document.querySelector('.onHold')
+        const canceledDiv = document.querySelector('.canceled')
+
         fetch("http://localhost:3000/todos")
-            .then(res =>{
-                return res.json()
+            .then(response => {
+                return response.json()
             })
-            .then(json =>{
-                const activeDiv = document.querySelector('.active')
-                const finishedDiv = document.querySelector('.finished')
-                const onHoldDiv = document.querySelector('.onHold')
-                const canceledDiv = document.querySelector('.canceled')
+            .then(json => {
 
                 let createCloseTodoBtn = document.createElement("button")
                 createCloseTodoBtn.classList.add("closeTodoBtn")
@@ -78,21 +69,23 @@ createTodoBtn.addEventListener("click", () =>{
                 if (activeRadio.checked && inputTodo.value != '') {
                     createTodo.append(createCloseTodoBtn)
                     activeDiv.append(createTodo)
-                }else if(finishedRadio.checked && inputTodo.value != ''){
+                } else if (finishedRadio.checked && inputTodo.value != '') {
                     createTodo.append(createCloseTodoBtn)
                     finishedDiv.append(createTodo)
-                }else if(onHoldRadio.checked && inputTodo.value != ''){
+                } else if (onHoldRadio.checked && inputTodo.value != '') {
                     createTodo.append(createCloseTodoBtn)
                     onHoldDiv.append(createTodo)
-                }else if(canceledRadio.checked && inputTodo.value != ''){
+                } else if (canceledRadio.checked && inputTodo.value != '') {
                     createTodo.append(createCloseTodoBtn)
                     canceledDiv.append(createTodo)
                 }
 
-                createCloseTodoBtn.addEventListener('click', function(event){
+
+
+                createCloseTodoBtn.addEventListener('click', function (event) {
                     event.stopPropagation()
                     createTodo.remove()
                 })
             })
-    }       
+    }
 })
