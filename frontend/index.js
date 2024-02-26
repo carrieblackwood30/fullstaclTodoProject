d4e233665952100245e25112e3f62cc3ccbc2a6d
Meirzhan <carrieblackwood30@gmail.com>
2024-02-26 20:37:39 +0600

    some news

--- frontend/index.js
+++ frontend/index.js
@@ -6,86 +6,90 @@ const finishedRadio = document.querySelector(".finishedRadio")
 const onHoldRadio = document.querySelector(".onHoldRadio")
 const canceledRadio = document.querySelector(".canceledRadio")
 
+const activeDiv = document.querySelector('.active')
+const finishedDiv = document.querySelector('.finished')
+const onHoldDiv = document.querySelector('.onHold')
+const canceledDiv = document.querySelector('.canceled')
+
 let todoArray = []
+
 
 const URL = "http://localhost:3000/todos"
 
-async function getTodos() {
+async function get_Todos() {
     try {
-        const response = await fetch(URL)
-        const data = response.json()
+        const resp = await fetch(URL)
+        const data = await resp.json()
         return data
-    } catch (e) {
-        return e
+    } catch (err) {
+        return err;
     }
 }
 
-async function postTodos() {
+let selectedRadio = () => {
+    let selected = document.querySelector("input[name = 'status']:checked").value
+
+    return selected
+}
+
+function display_Todos(todoArr) {
+
+    console.log(todoArr)
+
+    // let createCloseTodoBtn = document.createElement("button")
+    // createCloseTodoBtn.classList.add("closeTodoBtn")
+    // createCloseTodoBtn.textContent = 'X'
+
+
+
+    for (let i = 0; i < todoArr.length; i++) {
+
+        let createTodo = document.createElement('div')
+        createTodo.classList.add('todo')
+        createTodo.innerText += todoArr[i].name
+
+
+        if (todoArr[todoArr.length - 1].status == 'finished') {
+            finishedDiv.append(createTodo)
+        }
+    }
+
+
+
+}
+
+async function post_todos() {
     try {
         let options = {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                name: inputTodo.value
-            })
+                name: inputTodo.value,
+                status: selectedRadio()
+            }),
         }
-        const response = await fetch(URL, options)
-        const data = await response.json()
+        const resp = await fetch(URL, options)
+        const data = await resp.json()
         return data
-    } catch (e) {
-        return e
+    } catch (err) {
+        return err;
     }
 }
 
+get_Todos()
+    .then((todoArr) => {
+        todoArray = todoArr
+        console.log(todoArr)
+        display_Todos(todoArr)
+    })
+    .catch((err) => console.log(err))
 
-
-createTodoBtn.addEventListener("click", async () => {
-
-    await postTodos()
+createTodoBtn.addEventListener("click", () => {
 
     if (inputTodo.value != '') {
-
-        const activeDiv = document.querySelector('.active')
-        const finishedDiv = document.querySelector('.finished')
-        const onHoldDiv = document.querySelector('.onHold')
-        const canceledDiv = document.querySelector('.canceled')
-
-        fetch("http://localhost:3000/todos")
-            .then(response => {
-                return response.json()
-            })
-            .then(json => {
-
-                let createCloseTodoBtn = document.createElement("button")
-                createCloseTodoBtn.classList.add("closeTodoBtn")
-                createCloseTodoBtn.textContent = 'X'
-
-                let createTodo = document.createElement('div')
-                createTodo.classList.add('todo')
-                createTodo.textContent = json[json.length - 1].name
-
-                if (activeRadio.checked && inputTodo.value != '') {
-                    createTodo.append(createCloseTodoBtn)
-                    activeDiv.append(createTodo)
-                } else if (finishedRadio.checked && inputTodo.value != '') {
-                    createTodo.append(createCloseTodoBtn)
-                    finishedDiv.append(createTodo)
-                } else if (onHoldRadio.checked && inputTodo.value != '') {
-                    createTodo.append(createCloseTodoBtn)
-                    onHoldDiv.append(createTodo)
-                } else if (canceledRadio.checked && inputTodo.value != '') {
-                    createTodo.append(createCloseTodoBtn)
-                    canceledDiv.append(createTodo)
-                }
-
-
-
-                createCloseTodoBtn.addEventListener('click', function (event) {
-                    event.stopPropagation()
-                    createTodo.remove()
-                })
-            })
+        post_todos()
     }
+
 })
